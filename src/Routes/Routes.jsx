65df39fafdec5102960fe943c3bab5db5d9c23e9
Gazi2050 <gazi45344@gmail.com>
2024-02-16 17:54:50 +0600

    set-up project

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Root from "../Components/Root";
+import Home from "../Components/Home";
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <h1 className="text-5xl font-bold">Error</h1>,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+        ]
+    },
+]);
