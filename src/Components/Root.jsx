6c09193990507f171dbffe9d9f0cc9bf6b838db1
Gazi2050 <gazi45344@gmail.com>
2024-02-16 20:41:44 +0600

    navbar , home ,routes added

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const Root = () => {
     return (
         <div>
-            <Outlet />
+            <div className="flex justify-start">
+                <div>
+                    <Navbar />
+                </div>
+                <div>
+                    <Outlet />
+                </div>
+            </div>
         </div>
     );
 };
