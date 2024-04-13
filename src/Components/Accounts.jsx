7cf3215cf3e8d4c24841a127e1652b8c861f54cc
Gazi2050 <gazi45344@gmail.com>
2024-04-13 09:18:26 +0600

    update Accounts

--- src/Components/Accounts.jsx
+++ src/Components/Accounts.jsx
@@ -1,17 +1,19 @@
+import { Helmet } from "react-helmet-async";
 
 const Accounts = () => {
     return (
-        <div className="flex justify-center items-center px-52 ">
-            <div>
-                <div className="w-full max-w-md p-20 mt-16 bg-zinc-800 rounded-lg shadow-lg dark:bg-gray-800 shadow-purple-500">
-                    <div className="flex justify-center -mt-32">
-                        <img className="object-cover w-24 h-24 border-2 border-purple-500 rounded-full dark:border-blue-400 bg-white" alt="Testimonial avatar" src="assets/profile-user.png" />
+        <div className="lg:pt-5 mr-10 md:ml-36  lg:ml-80">
+            <Helmet>
+                <title>Hexa | Accounts</title>
+            </Helmet>
+            <div className="flex justify-center items-center pt-24">
+                <div className="w-full px-20 py-14 lg:px-28 lg:py-16 lg:mt-5 mb-5 bg-zinc-800 rounded-lg shadow-lg dark:bg-gray-800  shadow-purple-500 text-white">
+                    <div className="flex justify-center -mt-28 md:justify-center mb-10">
+                        <img className="object-cover w-auto h-auto md:w-28 lg:w-32 lg:h-32 border-[3px] border-purple-500 rounded-full dark:border-blue-400 bg-white" alt="Testimonial avatar" src='public/assets/profile-user.png' />
                     </div>
-
-                    <div className="space-y-5 -ml-11 mt-3">
-                        <h2 className="mt-2 text-xl font-semibold text-white dark:text-white md:mt-0">username :</h2>
-                        <h2 className="mt-2 text-xl font-semibold text-white dark:text-white md:mt-0">email :</h2>
-
+                    <div className="space-y-5 -ml-10">
+                        <h2 className="m-2 text-base font-semibold md:mt-0"><span className="font-bold">Name :</span> </h2>
+                        <h2 className="m-2 text-base font-semibold md:mt-0"><span className="font-bold">Email :</span> </h2>
                     </div>
                 </div>
             </div>
